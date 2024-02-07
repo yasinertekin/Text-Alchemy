a0b3abf87899d892200651e51336c7dd6d4ba4d3
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-07 14:46:52 +0300

    Add NavBar feature

---
+++ lib/feature/nav_bar/nav_bar.dart
@@ -0,0 +1,40 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:text_recognitions/product/router/app_router.gr.dart';
+
+@RoutePage()
+
+/// NavBar
+final class NavBar extends StatelessWidget {
+  /// Creates a [NavBar].
+  const NavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsScaffold(
+      routes: const [
+        ImagePickerRoute(),
+        PinnedRoute(),
+      ],
+      bottomNavigationBuilder: (_, tabsRouter) {
+        return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: tabsRouter.activeIndex,
+          onTap: tabsRouter.setActiveIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book_rounded),
+              label: 'Image Picker',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star_rate_rounded),
+              label: 'Pinned',
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
