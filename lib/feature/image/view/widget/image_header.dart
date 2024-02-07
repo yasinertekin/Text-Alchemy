64a2ff87ceffb9fd03508799de76b47d4a1efa97
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-07 14:45:23 +0300

    Add image header and pinned button widgets

---
+++ lib/feature/image/view/widget/image_header.dart
@@ -0,0 +1,57 @@
+part of '../image_picker_view.dart';
+
+final class _ImageHeader extends StatelessWidget {
+  const _ImageHeader({
+    required this.imagePickerViewModel,
+  });
+
+  final ImagePickerViewModel imagePickerViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        _PinnedButton(
+          imagePickerViewModel,
+        ),
+        CopyIconButton(
+          result: imagePickerViewModel.result ??
+              Result(
+                text: '',
+              ),
+        ),
+      ],
+    );
+  }
+}
+
+final class _PinnedButton extends StatelessWidget {
+  const _PinnedButton(
+    this.imagePickerViewModel,
+  );
+
+  final ImagePickerViewModel imagePickerViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostCacheManagerCubit, PinnedCacheManagerState>(
+      builder: (context, state) => Card(
+        child: IconButton(
+          onPressed: () {
+            if (imagePickerViewModel.result != null) {
+              if (state is PinnedCacheManagerLoadedState) {
+                context.read<PostCacheManagerCubit>().addPosts(
+                      imagePickerViewModel.result!,
+                    );
+              }
+            }
+          },
+          icon: const Icon(
+            Icons.push_pin_outlined,
+          ),
+        ),
+      ),
+    );
+  }
+}
