995e683677fcfb20ae6ddced7c0ab4ec2c4fd4b3
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-07 14:48:53 +0300

    Add image picker functionality

---
+++ lib/product/image_picker/image_picker.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:text_recognitions/product/image_picker/image_picker_interface.dart';
+
+final class ImagePickers extends ImagePickerInterface {
+  ImagePickers(super.imagePicker);
+
+  @override
+  Future<File?> pickImage() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      return File(pickedFile.path);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<File?> takePicture() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      return File(pickedFile.path);
+    } else {
+      return null;
+    }
+  }
+}
