cb34184cbfb7ea82454692b9bea09661293a26f0
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-08 23:53:15 +0300

    Refactor detail view and remove unnecessary imports

---
+++ lib/feature/detail/view/detail_view.dart
@@ -0,0 +1,43 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:text_recognitions/product/model/result.dart';
+import 'package:text_recognitions/product/router/app_router.gr.dart';
+import 'package:text_recognitions/product/widget/copy_icon_button.dart';
+
+part 'widget/detail_app_bar.dart';
+part 'widget/detail_tab_bar.dart';
+
+/// Detail view
+@RoutePage()
+final class DetailView extends StatelessWidget {
+  /// Constructor
+  const DetailView({
+    required this.result,
+    super.key,
+  });
+
+  /// Result model
+  final Result result;
+
+  @override
+  Widget build(BuildContext context) {
+    final Key myWidgetKey = UniqueKey();
+
+    return AutoTabsRouter.tabBar(
+      key: myWidgetKey,
+      routes: [
+        DetailTextFieldRoute(result: result),
+        ImageRoute(result: result),
+      ],
+      builder: (context, child, controller) {
+        return Scaffold(
+          appBar: _DetailAppBar(
+            result: result,
+            controller: controller,
+          ),
+          body: child,
+        );
+      },
+    );
+  }
+}
