fbcce4343418482a9d9a5f38fd7f23708e1948a0
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-08 23:26:31 +0300

    Add custom drawer and navigation app bar

--- lib/feature/nav_bar/nav_bar.dart
+++ lib/feature/nav_bar/nav_bar.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:text_recognitions/feature/pinned/view_model/pinned_cubit.dart';
+import 'package:text_recognitions/product/initialize/theme/cubit/theme_cubit.dart';
 import 'package:text_recognitions/product/router/app_router.gr.dart';
+
+part 'widget/custom_drawer.dart';
+part 'widget/nav_app_bar.dart';
 
 @RoutePage()
 
@@ -15,6 +21,11 @@ final class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final Key myWidgetKey = UniqueKey();
     return AutoTabsScaffold(
+      appBarBuilder: (_, tabsRouter) {
+        final currentTab = tabsRouter.activeIndex;
+        return _NavAppBar(currentTab: currentTab);
+      },
+      drawer: const _CustomDrawer(),
       key: myWidgetKey,
       routes: const [
         ImagePickerRoute(),
@@ -27,7 +38,7 @@ final class NavBar extends StatelessWidget {
           onTap: tabsRouter.setActiveIndex,
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.book_rounded),
+              icon: Icon(Icons.image_sharp),
               label: 'Image Picker',
             ),
             BottomNavigationBarItem(
