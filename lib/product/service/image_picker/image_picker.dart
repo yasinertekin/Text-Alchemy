9b52fdfe279695f3849c0dac0fda4d7df96a9488
Yasin Ertekin <yasinertekinn@outlook.com>
2024-02-09 16:47:06 +0300

    Refactor image picker classes

---
+++ lib/product/service/image_picker/image_picker.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:text_recognitions/product/service/image_picker/image_picker_interface.dart';
+import 'package:text_recognitions/product/utility/custom_exception.dart';
+
+/// ImagePickers
+final class ImagePickers extends ImagePickerInterface {
+  /// Constructor
+  ImagePickers(super.imagePicker);
+
+  @override
+  Future<File?> pickImage() async {
+    try {
+      final pickedFile =
+          await imagePicker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        return File(pickedFile.path);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      throw CustomException('Resim seçme hatası: $e');
+    }
+  }
+
+  @override
+  Future<File?> takePicture() async {
+    try {
+      final pickedFile =
+          await imagePicker.pickImage(source: ImageSource.camera);
+
+      if (pickedFile != null) {
+        return File(pickedFile.path);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      throw CustomException('Resim seçme hatası: $e');
+    }
+  }
+}
