9d3b491d79c95e83f964ff7f9022ac0d5d5739bd
Yasin <yasinertekin24@gmail.com>
2024-02-10 20:03:47 +0300

    Add VS Code settings and remove unused import in nav_bar.dart

--- lib/feature/nav_bar/nav_bar.dart
+++ lib/feature/nav_bar/nav_bar.dart
@@ -1,13 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kartal/kartal.dart';
 import 'package:text_recognitions/feature/pinned/view_model/pinned_cubit.dart';
 import 'package:text_recognitions/product/core/constants/string_constants.dart';
 import 'package:text_recognitions/product/initialize/theme/cubit/theme_cubit.dart';
 import 'package:text_recognitions/product/model/drawer/drawer_model.dart';
 import 'package:text_recognitions/product/router/app_router.gr.dart';
-import 'package:text_recognitions/product/widget/custom_sized_box.dart';
 
 part 'widget/custom_drawer.dart';
 part 'widget/nav_app_bar.dart';
