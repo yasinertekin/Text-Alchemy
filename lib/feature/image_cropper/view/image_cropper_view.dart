4d649b90cce0be29ee40638af0b0d529523df667
Yasin <yasinertekin24@gmail.com>
2024-02-10 16:25:43 +0300

    Add ImageCropperViewMixin and update ImageCropperView

--- lib/feature/image_cropper/view/image_cropper_view.dart
+++ lib/feature/image_cropper/view/image_cropper_view.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:text_recognitions/feature/image/view_model/image_pickar_view_model.dart';
+import 'package:text_recognitions/feature/image_cropper/view/mixin/image_cropper_view_mixin.dart';
 import 'package:text_recognitions/feature/image_cropper/view_model/image_cropper_view_model.dart';
 import 'package:text_recognitions/product/core/constants/string_constants.dart';
 import 'package:text_recognitions/product/widget/custom_image.dart';
@@ -16,9 +17,10 @@ part 'widget/process_image_button.dart';
 @RoutePage()
 
 /// ImageCropperView
-final class ImageCropperView extends StatelessWidget {
+final class ImageCropperView extends StatelessWidget
+    with ImageCropperViewMixin<ImageCropperView> {
   /// Constructor
-  const ImageCropperView({
+  ImageCropperView({
     required this.image,
     super.key,
   });
@@ -28,8 +30,12 @@ final class ImageCropperView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imageProvider = context.read<ImagePickerViewModel>();
+
     return ImageCropperBody(
       image: image,
+      viewModel: viewModel,
+      imageProvider: imageProvider,
     );
   }
 }
@@ -39,16 +45,22 @@ final class ImageCropperBody extends StatelessWidget {
   /// Constructor
   const ImageCropperBody({
     required this.image,
+    required this.viewModel,
+    required this.imageProvider,
     super.key,
   });
 
   /// Image
   final File image;
 
+  /// ViewModel
+  final ImageCropperViewModel viewModel;
+
+  /// ImageProvider
+  final ImagePickerViewModel imageProvider;
+
   @override
   Widget build(BuildContext context) {
-    final viewModel = ImageCropperViewModel();
-    final imageProvider = context.read<ImagePickerViewModel>();
     return ListenableBuilder(
       listenable: viewModel,
       builder: (context, child) => Scaffold(
