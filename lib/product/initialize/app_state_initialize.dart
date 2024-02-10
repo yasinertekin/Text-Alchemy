0bf76e71a1505dd518200cfd425446fa57b80ef1
Yasin <yasinertekin24@gmail.com>
2024-02-10 16:27:03 +0300

    Refactor imports and use dependency injection

--- lib/product/initialize/app_state_initialize.dart
+++ lib/product/initialize/app_state_initialize.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:text_recognitions/feature/image/view_model/image_pickar_view_model.dart';
-import 'package:text_recognitions/feature/pinned/view_model/pinned_cubit.dart';
-import 'package:text_recognitions/product/core/cache/pinned_manager.dart';
+import 'package:text_recognitions/locators.dart';
 import 'package:text_recognitions/product/initialize/theme/cubit/theme_cubit.dart';
-import 'package:text_recognitions/product/service/image_picker/image_picker.dart';
 
 /// AppStateInitialize
 final class AppStateInitialize extends StatelessWidget {
@@ -24,19 +21,13 @@ final class AppStateInitialize extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => TextRecognitionCacheManagerCubit(
-            PinnedManager.createCacheManager(),
-          ),
+          create: (context) => Locators.textRecognitionCacheManagerCubit,
         ),
         BlocProvider(
           create: (context) => ThemeCubit(),
         ),
         ChangeNotifierProvider<ImagePickerViewModel>(
-          create: (context) => ImagePickerViewModel(
-            ImagePickers(
-              ImagePicker(),
-            ),
-          ),
+          create: (context) => Locators.imagePickerViewModel,
         ),
       ],
       child: child,
