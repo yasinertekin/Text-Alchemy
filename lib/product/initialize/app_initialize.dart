ec006a77bb819c393504451e9eb64f8ae54c630a
Yasin <yasinertekin24@gmail.com>
2024-02-10 16:26:56 +0300

    Add Locators setup to AppInitialize

--- lib/product/initialize/app_initialize.dart
+++ lib/product/initialize/app_initialize.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:text_recognitions/locators.dart';
 
 @immutable
 
@@ -9,9 +10,11 @@ import 'package:path_provider/path_provider.dart';
 abstract final class AppInitialize {
   /// Initialize the app
   static Future<void> initialize() async {
+    await Hive.initFlutter();
+
     HydratedBloc.storage = await HydratedStorage.build(
       storageDirectory: await getApplicationDocumentsDirectory(),
     );
-    await Hive.initFlutter();
+    await Locators.setup();
   }
 }
